724f3b4af632fc6e2358637c1a8545e3b2f9c184
neonkitchen <neelan.pather@gmail.com>
2021-03-23 08:57:48 +0200

    seed

--- common/utils.py
+++ common/utils.py
@@ -298,8 +298,13 @@ def setup_logging(verbose):
 
 def initialize_seeds(seed):
     # set seeds
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
     np.random.seed(seed)
 
 
