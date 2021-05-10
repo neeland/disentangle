c7fe20f31a43cc7dca6ea4d2b6aa7a7af1b4a204
neonkitchen <neelan.pather@gmail.com>
2021-05-10 10:49:16 +0200

    cars

---
+++ 100b9.celeba.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH --job-name=100b9.celeba
+#SBATCH --output=/home-mscluster/npather/disentangle/logs/100b9.celeba.%A_%a.txt
+#SBATCH --partition=stampede
+#SBATCH --array=0-3
+python3 main.py \
+--aicrowd_challenge=false \
+--name=100b9.celeba \
+--alg=BetaVAE \
+--traverse_z=true \
+--traverse_c=true \
+--dset_dir=/home-mscluster/npather/disentanglement-pytorch/data/celeba/ \
+--dset_name=celebA \
+--encoder=PadlessGaussianConv64 \
+--decoder=SimpleConv64 \
+--discriminator=SimpleDiscriminator \
+--z_dim=20 \
+--use_wandb=true \
+--w_kld=9.0 \
+--w_tc=0.0 \
+--lr_G=0.001 \
+--lr_scheduler=ReduceLROnPlateau \
+--lr_scheduler_args mode=min factor=0.95 patience=1 min_lr=0.00005 \
+--max_iter=200000 \
+--iterations_c=200000 \
+--evaluate_iter=10000 \
+--recon_iter=10000 \
+--traverse_iter=10000 \
+--print_iter=10000 \
+--evaluation_metric mig sap_score irs dci beta_vae_sklearn factor_vae_metric \
+--alg=BetaVAE \
+--loss_terms \
+--use_bandit=false \
+--controlled_capacity_increase=false \
+--loss_txt=false \
+--wandb_project_name=celeba \
+--jeffreys=false \
+--w_jeff=-1.0 \
+
+
